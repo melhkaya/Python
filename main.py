a2d6df3504c8db4b6403b4cbf8c8f2725dc42b21
Melih <melih.kaya@etiya.com>
2022-11-17 02:35:00 +0300

    Day1 Self Study

--- main.py
+++ main.py
@@ -1,11 +1,55 @@
-number1 = 25
-number2 = 25
+print ('merhaba')
 
-if number1>number2:
-    print (number1)
-
-elif number1<number2:
-    print (number2)
+text = '10'
+print (text)
 
 
+#integer 
+number= 10 
+print (number) 
 
+#double,float,decimal
+numberd = 6.7
+print (numberd)
+
+
+print(number + 5)
+print(number -5)
+print (number * 5)
+print (number / 5)
+
+
+#boolean, bool = true - false
+isVerified = True
+
+
+
+#string operasyonları
+text = 'merhaba'
+print (text.upper())
+print (text.lower())
+print (text.startswith('mer'))
+print (text.endswith('etiya'))
+
+
+name = 'melih'
+age = 29
+company = 'Etiyada'
+
+print (name+ ' ' +age + 'yaşinda' ' '+ company +' ' + 'çalişiyor')
+
+
+
+baslik = 'Haberiniz Olsun'
+vade = 12 
+faizOranı = 1.47
+
+print (type(baslik))
+print (type(vade))
+print (type(faizOranı))
+
+mesaj = 'Hoşgeldin'
+musteriAdi = 'Melih'
+musteriSoyadı = 'Kaya'
+sonucMesaj = mesaj+' '+musteriAdi+' '+musteriSoyadı
+print (sonucMesaj)
